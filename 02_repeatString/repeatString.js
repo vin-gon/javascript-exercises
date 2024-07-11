12138782708f536148231c4bff7e846ededf87d4
Arvin Gonzales <arvingonz@gmail.com>
2024-07-11 15:55:31 -0700

    Complete exercises 1-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0)
+        return 'ERROR';
+    
+    let res = '';
+    for (let i = 0; i < num; ++i){
+        res += str;
+    }
 
+    return res;
 };
 
 // Do not edit below this line
