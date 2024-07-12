1157f29fdf903d483abdf2d82e3d5d3976ccb9ea
ArvinGonzales <ArvinGonz@gmail.com>
2024-07-11 23:26:13 -0700

    Complete exercises 3-5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let res = '';
+    for (let i = str.length - 1; i >= 0; --i)
+    {
+        res += str[i];
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let res = [];
+    for (let i = 0; i < arr.length; ++i)
+    {
+        let removeElement = false;
+        for (let j = 0; j < args.length; ++j)
+        {
+            if (arr[i] === args[j])
+            {
+                removeElement = true;
+            }
+        }
+        if (!removeElement)
+        {
+            res.push(arr[i]);
+        }
+    }
+
+    return res;
 };
 
 // Do not edit below this line
